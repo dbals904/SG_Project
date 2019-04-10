8638aaee787d7600328e5d83a95854d9f5a30c52
YuMin <dbals904@gmail.com>
2019-04-10 22:54:33 +0900

    후

--- app/src/main/java/com/graduate/seoil/sg_projdct/SettingActivity.java
+++ app/src/main/java/com/graduate/seoil/sg_projdct/SettingActivity.java
@@ -2,8 +2,11 @@ package com.graduate.seoil.sg_projdct;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -11,6 +14,7 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
 
         FirebaseAuth.getInstance().signOut();
     }
